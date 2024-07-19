ffccce5d8383ad7d638ae130d9f886c003ed728c
dogadinm <dogadin01@gmail.com>
2024-07-19 14:49:28 +0200

    -h

---
+++ CPP_Module_08/ex02/MutantStack.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+private:
+
+public:
+    typedef typename std::stack<T>::container_type::iterator iterator;
+
+    MutantStack() : std::stack<T>(){
+        // std::cout << "Default Constructor called" << std::endl;
+    }
+    MutantStack(MutantStack   const &copy) : std::stack<T>(copy){
+        // std::cout << "Copy Constructor called" << std::endl;
+    }
+    ~MutantStack(){
+        // std::cout << "Deconstuctor called" << std::endl;
+    }
+
+    MutantStack  &operator=(MutantStack  const &copy)
+    {
+        if (*this != copy)
+            *this = copy;
+        return (*this);
+    }
+
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
+
+};
+
